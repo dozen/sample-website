fac8f4da7fd8925b0cb3a8a0e63ba9507b3fd602
dozen <iwatakiosk@gmail.com>
2016-10-03 16:16:29 +0900

    外部キー制約を消す

--- sql/create.sql
+++ sql/create.sql
@@ -16,21 +16,16 @@ id      INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title   VARCHAR(255)    NOT NULL,
 user_id INT             NOT NULL,
 content TEXT            NOT NULL,
-FOREIGN KEY (user_id) REFERENCES users(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE favorites (
 id          INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
 article_id  INT             NOT NULL,
 user_id     INT             NOT NULL,
-FOREIGN KEY (article_id)    REFERENCES articles(id),
-FOREIGN KEY (user_id)       REFERENCES users(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE followings (
 id              INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
 from_id         INT             NOT NULL,
 to_id           INT             NOT NULL,
-FOREIGN KEY     (from_id)       REFERENCES users(id),
-FOREIGN KEY     (to_id)         REFERENCES users(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
